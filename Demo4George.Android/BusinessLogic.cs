cdfb78b6d8ab2adedd8a8ce4c25f1c568e0fdaf3
shoddy <hodkinsons@gmail.com>
2014-06-05 22:44:57 +1000

    Absolutely the most basic app ever

---
+++ Demo4George.Android/BusinessLogic.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace Demo4George.Android
+{
+	public class BusinessLogic
+	{
+		public string[] GetAutoCompleteText()
+		{
+			// Use a web service if that floats your boat...
+			return new string[] { "the who", "the sex pistols", "the ramones" };
+		}
+
+
+		public async Task<string> GetSearchResults(string searchText)
+		{
+			// Fake a slow web service call...
+			await Task.Delay (3000);
+
+			//
+			using (var webClient = new WebClient ()) {
+				var url = string.Format (@"http://musicbrainz.org/ws/2/artist/?query=artist:{0}", Uri.EscapeUriString (searchText));
+				Console.WriteLine ("Searching: {0}", url);
+								
+				return await webClient.DownloadStringTaskAsync (url);
+			}
+		}
+	}
+}
+
---
+++ Demo4George.Android/MainActivity.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace Demo4George.Android
+{
+	[Activity (Label = "Demo4George.Android", MainLauncher = true)]
+	public class MainActivity : Activity
+	{
+		BusinessLogic businessLogic = new BusinessLogic();
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			// Load the autocomplete text (static for now -- it could be from a web service)...
+			AutoCompleteTextView autoCompleteSearch = FindViewById<AutoCompleteTextView> (Resource.Id.autoCompleteSearch);
+			autoCompleteSearch.Adapter = new ArrayAdapter<String> (this, Resource.Layout.SearchResultListItem, businessLogic.GetAutoCompleteText());
+
+			Button searchButton = FindViewById<Button> (Resource.Id.searchButton);
+			TextView resultsText = FindViewById<TextView> (Resource.Id.resultsText);
+			TextView searchingText = FindViewById<TextView> (Resource.Id.searchingText);
+
+			searchButton.Click += async (sender, e) => {
+				Console.WriteLine("Search clicked: {0}", autoCompleteSearch.Text);
+
+				searchButton.Enabled = false;
+				searchingText.Visibility = ViewStates.Visible;
+
+				resultsText.Text = await businessLogic.GetSearchResults(autoCompleteSearch.Text);
+
+				searchingText.Visibility = ViewStates.Gone;
+				resultsText.Visibility = ViewStates.Visible;
+				searchButton.Enabled = true;
+			};
+
+		}
+	}
+}
+
+
